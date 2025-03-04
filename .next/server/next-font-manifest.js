b01939b335894a1ebeb56a217005a43e3864c44c
Aayush Kapoor <61514758+Aayush-K15@users.noreply.github.com>
2025-03-05 00:04:07 +0530

    Removed .next from tracking

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/aayushkapoor/Projectss/mentorhub/mentorhub/MentorHub/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
